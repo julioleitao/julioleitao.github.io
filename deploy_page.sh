a65af7fff2ba6f52c6b4b220adeac2cac282c478
Júlio Leitão <julio.leitao.jr@gmail.com>
2017-09-07 11:52:10 -0300

    adding deploy script

--- deploy_page.sh
+++ deploy_page.sh
@@ -10,8 +10,8 @@ then
         git worktree prune
         rm -rf .git/worktrees/public/
 
-        echo "Checking out gh-pages branch into public"
-        git worktree add -B gh-pages public upstream/gh-pages
+        echo "Checking out master branch into public"
+        git worktree add -B master public origin/master
 
         echo "Removing existing files"
         rm -rf public/*
@@ -19,5 +19,5 @@ then
         echo "Generating site"
         hugo
 
-        echo "Updating gh-pages branch"
-        cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+        echo "Updating master branch"
+        cd public && git add --all && git commit -m "Publishing to master (publish.sh)"
